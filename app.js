d18d2e79c2c200b6dcda9999ea35a5b9b01140c9
Sean <18058799212@163.com>
2018-03-22 14:14:15 +0800

    update

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const Koa = require("koa");
 const app = new Koa();
 
 app.use(async (ctx, next) => {
+    // console.error(ctx)
     await next();
     ctx.response.type = "text/html";
     ctx.response.body = "<h1>Hello, koa2!</h1>";
 });
 
-app.on("error", err => {
-    log.error("server error", err);
-});
+// app.on("error", err => {
+//     console.error(err)
+//     // log.error("server error", err);
+// });
 
-app.listen(6666);
+app.listen(3000);
 console.log("======= seed start ========");
-console.log("======= app started at port 6666 ========");
+console.log("======= app started at port 3000 ========");
