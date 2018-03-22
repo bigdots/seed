f317f1f0fb88e59ede01c7c147d7e3a213aa6a59
Sean <18058799212@163.com>
2018-03-22 15:39:09 +0800

    add render-engine swing

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const Koa = require("koa");
-const Router = require('koa-router');
+const Router = require("koa-router");
+const swig = require("swig");
+const path = require("path");
 
 const app = new Koa();
 let router = new Router();
 
-router.get('/', (ctx, next)=>{
+app.context.render = (url, data = null, ctx) => {
+    let template = swig.compileFile(path.resolve(__dirname, "src/view/", url));
+    ctx.response.type = "text/html";
+    ctx.response.body = template(data);
+};
+
+router.get("/", (ctx, next) => {
     ctx.response.type = "text/html";
     ctx.response.body = "<h1>Hello, seed!</h1>";
-})
+});
+
+router.get("/add", (ctx, next) => {
+    ctx.render(
+        "add.html",
+        {
+            pagename: "awesome people",
+            authors: ["Paul", "Jim", "Jane"]
+        },
+        ctx
+    );
+});
 
 app.use(router.routes());
 
 app.listen(3000);
 
-console.log("======= seed start ========");
-console.log("======= app started at port 3000 ========");
+console.log("======= seed started at port 3000 ========");
